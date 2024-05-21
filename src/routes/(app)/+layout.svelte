c7fa41a4e4e48087ee20cf5934baae36b63e1b26
jiangxiaoyu <1047647281@qq.com>
2024-05-21 23:49:54 +0800

    feat: 本地调试成功

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -193,7 +193,7 @@
 
 <ShortcutsModal bind:show={showShortcuts} />
 <SettingsModal bind:show={$showSettings} />
-<ChangelogModal bind:show={$showChangelog} />
+<!-- <ChangelogModal bind:show={$showChangelog} /> -->
 
 <div class="app relative">
 	<div
