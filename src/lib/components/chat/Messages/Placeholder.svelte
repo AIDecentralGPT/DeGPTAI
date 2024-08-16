39f3ebe07a4ee100f2ed7ffb8e700abbf4eec7e9
jiangxiaoyu <1047647281@qq.com>
2024-08-16 16:29:29 +0800

    feat: 跳转x.degpt

--- src/lib/components/chat/Messages/Placeholder.svelte
+++ src/lib/components/chat/Messages/Placeholder.svelte
@@ -111,7 +111,7 @@
 		</div>
 
 
-		<a class="flex flex-col w-fit cursor-pointer border border-gray-600 rounded-lg p-4 py-2 mt-8  "  href="https://x.degpt.ai" target="_blank">
+		<a class="flex flex-col w-fit cursor-pointer border border-gray-600 rounded-lg p-4 py-2 mt-8  "  href="https://x.degpt.ai?comefrom=degpt.ai" target="_blank">
 			<svg xmlns="http://www.w3.org/2000/svg" width="8em" height="8em" viewBox="0 0 16 16"><path fill="currentColor" d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07l-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/></svg>
 			<div style="color: rgba(184, 142, 86, 1)">
 				Twitter Personality
