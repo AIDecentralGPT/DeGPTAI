b124751a52f80dcb89b1eb00ef043c01f9df7f3e
unknown <240892907@qq.com>
2024-09-30 07:44:12 +0800

    kyc认证失败修改

--- backend/apps/web/routers/auths.py
+++ backend/apps/web/routers/auths.py
@@ -929,7 +929,7 @@ async def faceliveness_check_for_ws(id: str):
                     }
             else:
                 # 添加人脸样本
-                await face_lib.add_face_sample(user.id)
+                face_lib.add_face_sample(user.id)
                 # 在人脸样本添加对应的人脸数据
                 face_id = face_lib.add_face_data(faceImg, user.id)   
             
