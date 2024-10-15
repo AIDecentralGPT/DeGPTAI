156bf95d8fb0d38846387172e679b2076e07844d
WIN-DZY\DZY <240892907@qq.com>
2024-10-16 01:25:08 +0800

    兼容配置

--- vite.config.ts
+++ vite.config.ts
@@ -20,9 +20,30 @@ export default defineConfig({
 	plugins: [
 		sveltekit(),
 		legacy({
-			targets: ["Chrome 64"],
-			modernPolyfills: true,
-		}),
+			targets: ['chrome 52'],
+			additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+			renderLegacyChunks: true,
+			polyfills: [
+			  'es.symbol',
+			  'es.promise',
+			  'es.promise.finally',
+			  'es/map',
+			  'es/set',
+			  'es.array.filter',
+			  'es.array.for-each',
+			  'es.array.flat-map',
+			  'es.object.define-properties',
+			  'es.object.define-property',
+			  'es.object.get-own-property-descriptor',
+			  'es.object.get-own-property-descriptors',
+			  'es.object.keys',
+			  'es.object.to-string',
+			  'web.dom-collections.for-each',
+			  'esnext.global-this',
+			  'esnext.string.match-all'
+			]
+		  }    
+		)
 	],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version)
