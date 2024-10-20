1ad6fe1672306cf20a80f89ce68722d6d015ee3f
WIN-DZY\DZY <240892907@qq.com>
2024-10-20 11:31:24 +0800

    复制方法修改

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -197,44 +197,28 @@ export const generateInitialsImage = (name) => {
 
 export const copyToClipboard = async (text) => {
 	let result = false;
-	if (!navigator.clipboard) {
-		const textArea = document.createElement('textarea');
-		textArea.value = text;
+	const textArea = document.createElement('textarea');
+	textArea.value = text;
 
-		// Avoid scrolling to bottom
-		textArea.style.top = '0';
-		textArea.style.left = '0';
-		textArea.style.position = 'fixed';
+	// Avoid scrolling to bottom
+	textArea.style.top = '0';
+	textArea.style.left = '0';
+	textArea.style.position = 'fixed';
 
-		document.body.appendChild(textArea);
-		textArea.focus();
-		textArea.select();
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
 
-		try {
-			const successful = document.execCommand('copy');
-			const msg = successful ? 'successful' : 'unsuccessful';
-			console.log('Fallback: Copying text command was ' + msg);
-			result = true;
-		} catch (err) {
-			console.error('Fallback: Oops, unable to copy', err);
-		}
-
-		document.body.removeChild(textArea);
-		return result;
+	try {
+		const successful = document.execCommand('copy');
+		const msg = successful ? 'successful' : 'unsuccessful';
+		console.log('Fallback: Copying text command was ' + msg);
+		result = true;
+	} catch (err) {
+		console.error('Fallback: Oops, unable to copy', err);
 	}
 
-	result = await navigator.clipboard
-		.writeText(text)
-		.then(() => {
-			console.log('Async: Copying to clipboard was successful!');
-			return true;
-		})
-		.catch((error) => {
-			console.error('Async: Could not copy text: ', error);
-			alert(error);
-			return false;
-		});
-
+	document.body.removeChild(textArea);
 	return result;
 };
 
