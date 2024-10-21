be4ae2ef85c5ce374add6fa846b90a023bb7b913
PC-20241021WOVA\Administrator <240892907@qq.com>
2024-10-22 07:11:05 +0800

    签到页面修改

--- src/lib/components/wallet/GetRewards.svelte
+++ src/lib/components/wallet/GetRewards.svelte
@@ -24,7 +24,7 @@
     {
       id: "new_wallet",
       text: $i18n.t("Create Wallet"),
-      reward: "2000 DGC",
+      reward: "1000 DGC",
       icon: '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M6 20q-1.65 0-2.825-1.175T2 16V8q0-1.65 1.175-2.825T6 4h12q1.65 0 2.825 1.175T22 8v8q0 1.65-1.175 2.825T18 20zM6 8h12q.55 0 1.05.125t.95.4V8q0-.825-.587-1.412T18 6H6q-.825 0-1.412.588T4 8v.525q.45-.275.95-.4T6 8m-1.85 3.25l11.125 2.7q.225.05.45 0t.425-.2l3.475-2.9q-.275-.375-.7-.612T18 10H6q-.65 0-1.137.338t-.713.912"/></svg>',
     },
     {
@@ -136,14 +136,13 @@
   
                 if (item.id === "new_wallet") {
                   $showNewWalletModal = true;
-                  return
                 }
   
-                if (item.text === "Share") {
+                else if (item.id === "invite") {
                   $showShareModal = true;
                 }
   
-                if (item.text === "Clock In") {
+                else if (item.id === "clock_in") {
                   if ($chats.length > 0) {
                     clockLoading = true;
                     await clockIn(localStorage.token)
@@ -163,12 +162,9 @@
                     clockLoading = false;
                   } else {
                     toast.warning("You need to complete a conversation to receive a reward ！");
-                  }
-
-                  
+                  }   
                 }
-  
-  
+                return;
               }}
             >
               {(($user?.id?.startsWith("0x") && rewardsCount[item.id]) || 0) > 0
