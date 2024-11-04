62cd58bf718f0fb249cbede99b5bc50d33e4c95e
PC-20241021WOVA\Administrator <240892907@qq.com>
2024-11-04 15:59:22 +0800

    更改模型名称

--- src/lib/apis/de/index copy 2.ts
+++ src/lib/apis/de/index copy 2.ts
@@ -1,5 +1,7 @@
 import { DE_API_BASE_URL } from "$lib/constants";
 import { promptTemplate } from "$lib/utils";
+import { models, settings, user } from "$lib/stores";
+import { get } from "svelte/store";
 
 function transformModelData(input) {
   const models = input?.data?.map((model) => {
@@ -29,6 +31,9 @@ function transformModelData(input) {
 
 // 获取De的所有模型列表
 export const getDeModels = async (token: string = "") => {
+  let error = null;
+
+  const host = "https://chat.degpt.ai";
 
   // const host =
   //   window.location.hostname === "localhost"
@@ -59,11 +64,17 @@ export const getDeModels = async (token: string = "") => {
           quantization_level: "",
         },
         expires_at: "0001-01-01T00:00:00Z",
+        urls: [
+          // "https://chat.degpt.ai/llama3-70b/v1"
+          host + "/api",
+          // "http://122.99.183.53:1042/v1"
+        ],
         source: "Exemplary performance,low error rate,diverse responses.",
+        nodeList: ["16Uiu2HAmBcP2Zv51z4VA8UnHRNRjatyHcv4TSuU6pXixLELP1U7F"],
       },
       {
         name: "Ali LLM (Qwen2-72B)",
-        model: "Qwen2.5-72B",
+        model: "Qwen2-72B",
         modified_at: new Date().toISOString(),
         size: 0, // 需要根据实际数据填充
         digest: "", // 需要根据实际数据填充
@@ -76,12 +87,19 @@ export const getDeModels = async (token: string = "") => {
           quantization_level: "",
         },
         expires_at: "0001-01-01T00:00:00Z",
+        urls: [
+          // "https://chat.degpt.ai/qwen2-72b/v1"
+          host + "/api",
+          // "http://122.99.183.53:1042/v1"
+        ],
         source: "27 language support, surpport long texts of up to 128 tokens.",
+        nodeList: ["16Uiu2HAmPKuJU5VE2PCnydyUn1VcTN2Lt59UDJFFEiRbb7h1x4CV"],
       },
 
       {
         name: "Google LLM (Gemma-2-27B)",
-        model: "NVLM-D-72B",
+
+        model: "Gemma-2-27B",
         modified_at: new Date().toISOString(),
         size: 0, // 需要根据实际数据填充
         digest: "", // 需要根据实际数据填充
@@ -94,7 +112,13 @@ export const getDeModels = async (token: string = "") => {
           quantization_level: "",
         },
         expires_at: "0001-01-01T00:00:00Z",
+        urls: [
+          // "https://chat.degpt.ai/llama3-70b/v1"
+          host + "/api",
+          // "http://122.99.183.53:1042/v1"
+        ],
         source: "Exemplary performance,low error rate,diverse responses.",
+        nodeList: ["16Uiu2HAmPKuJU5VE2PCnydyUn1VcTN2Lt59UDJFFEiRbb7h1x4CV"],
       },
 
       // Mistral LLM(Large2-123B)
@@ -113,7 +137,9 @@ export const getDeModels = async (token: string = "") => {
           quantization_level: "",
         },
         expires_at: "0001-01-01T00:00:00Z",
+        urls: [host + "/api"],
         source: "Code completion and generation, error detection and repair.",
+        nodeList: ["16Uiu2HAmPKuJU5VE2PCnydyUn1VcTN2Lt59UDJFFEiRbb7h1x4CV"],
       },
     ],
   };
@@ -124,6 +150,53 @@ export const getDeModels = async (token: string = "") => {
   }));
 };
 
+// // 获取De的所有模型列表
+// export const getDeModels = async (token: string = '') => {
+// 	let error = null;
+
+// 	const res = await fetch(`${DE_API_BASE_URL}/models`, {
+// 		method: 'GET',
+// 		headers: {
+// 			Accept: 'application/json',
+// 			'Content-Type': 'application/json',
+// 			...(token && { authorization: `Bearer ${token}` })
+// 		}
+// 	})
+// 		.then(async (res) => {
+// 			// console.log(123, await res.json());
+
+// 			// if (!res.ok) throw await res.json();
+// 			// console.log("res.json();", res.json(), res);
+
+// 			return res.json();
+
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 			if ('detail' in err) {
+// 				error = err.detail;
+// 			} else {
+// 				error = 'Server connection failed';
+// 			}
+// 			return null;
+// 		});
+
+// 		console.log("res", res);
+
+// 	const format_res = transformModelData(res)
+// 	console.log(format_res);
+
+// 	if (error) {
+// 		throw error;
+// 	}
+
+// 	return (format_res?.models ?? [])
+// 		.map((model) => ({ id: model.model, name: model.name ?? model.model, ...model }))
+// 		.sort((a, b) => {
+// 			return a.name.localeCompare(b.name);
+// 		});
+// };
+
 // 和De的模型对话
 export const generateDeOpenAIChatCompletion = async (
   token: string = "",
@@ -131,13 +204,20 @@ export const generateDeOpenAIChatCompletion = async (
   url: string = DE_API_BASE_URL
 ): Promise<[Response | null, AbortController]> => {
   const controller = new AbortController();
-  setTimeout(() => controller.abort(), 120000);
+  setTimeout(() => controller.abort(), 30000);
 
   let error = null;
 
   // console.log("generateDeOpenAIChatCompletion url:", url,`${url}/v0/chat/completion`, body);
 
-  const res = await fetch(`${url}/v0/chat/completion/proxy`, {
+  /**获取node列表 */
+  const model = body?.model;
+  const globalModelsSetted = get(models);
+  const nodeList = globalModelsSetted?.filter((item) => {
+    return item?.model === model;
+  })?.[0]?.nodeList;
+
+  const res = await fetch(`${url}/v0/chat/completion`, {
     signal: controller.signal,
     method: "POST",
     headers: {
@@ -147,6 +227,7 @@ export const generateDeOpenAIChatCompletion = async (
     body: JSON.stringify({
       ...body,
       project: "DecentralGPT",
+      node_id: nodeList?.[0],
       stream: true,
     }),
   }).catch((err) => {
@@ -175,7 +256,15 @@ export const generateDeTitle = async (
 
   console.log("generateDeTitle", url);
 
-  const res = await fetch(`${url}/v0/chat/completion/proxy`, {
+  /**获取node列表 */
+  const globalModelsSetted = get(models);
+  const nodeList = globalModelsSetted?.filter((item) => {
+    return item?.model === model;
+  })?.[0]?.nodeList;
+
+  console.log("generateDeTitle 222", nodeList);
+
+  const res = await fetch(`${url}/v0/chat/completion`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -185,6 +274,7 @@ export const generateDeTitle = async (
     },
     body: JSON.stringify({
       model: model,
+      node_id: nodeList?.[0],
       messages: [
         {
           role: "user",
