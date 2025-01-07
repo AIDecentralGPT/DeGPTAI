6cbe3154d000f1cf7bba7893ebaaa60a28635ea6
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-01-08 01:21:22 +0800

    更新合约地址

--- backend/apps/web/routers/users.py
+++ backend/apps/web/routers/users.py
@@ -30,7 +30,8 @@ from utils.misc import parse_duration, validate_email_format
 
 # --------钱包相关--------
 from web3 import Web3
-w3 = Web3(Web3.HTTPProvider('https://rpc-testnet.dbcwallet.io'))  # 使用以太坊主网
+#w3 = Web3(Web3.HTTPProvider('https://rpc-testnet.dbcwallet.io'))  # 旧以太坊主网
+w3 = Web3(Web3.HTTPProvider('https://rpc.dbcwallet.io')) # 新以太坊主网
 # from web3.auto import w3
 import asyncio
 
