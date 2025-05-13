ac0dd7ec9eed34fa1185b94ffc655ae6181ab9e9
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-05-13 20:17:50 +0800

    update code

--- backend/apps/web/routers/auths.py
+++ backend/apps/web/routers/auths.py
@@ -743,7 +743,7 @@ async def face_liveness(form_data: FaceLivenessRequest, user=Depends(get_current
             "bioMetaInfo": form_data.metaInfo.bioMetaInfo,
             "user_id": user.id
         })
-        
+            
         merchant_biz_id = response["merchant_biz_id"]
         transaction_id = response["transaction_id"]
         transaction_url = response["transaction_url"]
