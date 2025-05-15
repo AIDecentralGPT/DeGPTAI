661f80eef40a2d8111d8ef1bfd6ac1c76c242877
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-05-15 21:25:06 +0800

    update model url proxy2 to proxy

--- src/lib/apis/de/index.ts
+++ src/lib/apis/de/index.ts
@@ -216,7 +216,7 @@ const getDeOpenAIChatCompletion = async (
       throw new Error('timeout');
     }, 10000);
   }
-  res = await fetch(`${urlObj.url}/chat/completion/proxy2`, {
+  res = await fetch(`${urlObj.url}/chat/completion/proxy`, {
     signal: controller.signal,
     method: "POST",
     headers: {
@@ -261,7 +261,7 @@ export const generateDeTitle = async (
   model = 'qwen3-235b-a22b';
   for (const domain of urls) {
     try {
-      const result = await fetch(`${domain.url}/chat/completion/proxy2`, {
+      const result = await fetch(`${domain.url}/chat/completion/proxy`, {
         method: "POST",
         headers: {
           Accept: "application/json",
@@ -325,7 +325,7 @@ export const generateSearchKeyword = async (
   let model = 'qwen3-235b-a22b';
   for (const domain of urls) {
     try {
-      const result = await fetch(`${domain.url}/chat/completion/proxy2`, {
+      const result = await fetch(`${domain.url}/chat/completion/proxy`, {
         method: "POST",
         headers: {
           Accept: "application/json",
