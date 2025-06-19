057e4517ebeed5f32d7506b017e0b6c748006b6a
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-06-19 17:19:10 +0800

    upload icon path

--- src/routes/kyc/+page.svelte
+++ src/routes/kyc/+page.svelte
@@ -1,42 +1,55 @@
 <script lang="ts">
   import { onMount, getContext } from 'svelte';
   import { copyToClipboard } from "$lib/utils";
-  import { faceUrl } from "$lib/stores"
+  import { faceUrl, user } from "$lib/stores"
   import { toast } from "svelte-sonner";
+  import { getUserInfo } from "$lib/apis/users";
+  import { goto } from '$app/navigation';
+
   const i18n = getContext('i18n');
 
   onMount(() => {
   
   });
 
+  // 上一页
+  async function gotoLastPage() {
+    goto("/");
+  }
+
   // 返回首页
-  function gotoHome() {
-    window.location.href = "https://test.degpt.ai"
+  async function gotoHome() {
+    const userInfo = await getUserInfo(localStorage.token);
+    await user.set({
+      ...$user,
+      verified: userInfo?.verified,
+    });
+    goto("/");
   }
 
 </script>
 
 <div class="w-full flex flex-col">
   <div class="flex flex-col items-center w-full mt-8">
-    <img class="w-[200px]" src="./icon/faceError.png" alt="icon"/>
+    <img class="w-[200px]" src="./static/icon/faceError.png" alt="icon"/>
     <span class="font-bold mt-2">{$i18n.t("KYC Certification Instructions")}</span>
   </div>
   <div class="flex flex-col mt-6 m-2 bg-gray-100 p-4 rounded-md">
     <span class="text-">{$i18n.t("Please follow the steps below:")}</span>
     <div class="flex flex-row items-center mt-2">
-      <img class="size-8" src="./icon/copytip.png" alt=""/>
+      <img class="size-8" src="./static/icon/copytip.png" alt=""/>
       <span class="text-sm ml-2">{$i18n.t("Click \"Copy Link\".")}</span>
     </div>
     <div class="flex flex-row items-center mt-2">
-      <img class="size-8" src="./icon/browser.png" alt=""/>
+      <img class="size-8" src="./static/icon/browser.png" alt=""/>
       <span class="text-sm ml-2">{$i18n.t("Open your browser. Chrome, Firefox, Microsoft Edge and Safari on iOS 14.3 or later are all supported.")}</span>
     </div>
     <div class="flex flex-row items-center mt-2">
-      <img class="size-8" src="./icon/miniocr.png" alt=""/>
+      <img class="size-8" src="./static/icon/miniocr.png" alt=""/>
       <span class="text-sm ml-2">{$i18n.t("Paste the copied link and enable identity certificate recognition.")}</span>
     </div>
     <div class="flex flex-row items-center mt-2">
-      <img class="size-8" src="./icon/over.png" alt=""/>
+      <img class="size-8" src="./static/icon/over.png" alt=""/>
       <span class="text-sm ml-2">{$i18n.t("After authentication, return to the APP and click \"Complete Authentication\".")}</span>
     </div>
     <button class="primaryButton ml-2 text-sm text-white px-4 py-2 rounded-lg mt-6"
