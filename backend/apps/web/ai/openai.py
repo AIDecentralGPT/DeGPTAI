fae58cb1b6d53f182058600ab15d8fa0868feb52
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-07-02 18:01:05 +0800

    update gpt add sys message

--- backend/apps/web/ai/openai.py
+++ backend/apps/web/ai/openai.py
@@ -1,10 +1,11 @@
 import os
 from openai import OpenAI, APIError
-from apps.web.models.aimodel import AiModelReq
+from apps.web.models.aimodel import AiModelReq, AiMessageModel
 
 apikey = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=apikey)
 
+sysmessage = [AiMessageModel(role="system", content="You are ChatGPT, a helpful, honest, and polite AI assistant developed by OpenAI. You answer questions clearly, provide explanations when needed, and are always respectful and concise. If you are unsure about something, you say so. You aim to be as useful as possible without pretending to know things you don’t.")]
 
 class OpenAiApi:
     def check_model(self, model: str):
@@ -12,18 +13,20 @@ class OpenAiApi:
         return model in models
    
     def completion(self, param: AiModelReq):
+        messages = sysmessage + param.messages
+        print("====================", messages)
         try:
             if param.enable_thinking:
                 completion = client.chat.completions.create(
                     model=param.model,
                     reasoning_effort="medium",
-                    messages=param.messages,
+                    messages=messages,
                     stream=param.stream
                 )
             else:
                 completion = client.chat.completions.create(
                     model=param.model,
-                    messages=param.messages,
+                    messages=messages,
                     stream=param.stream,
                     temperature=0.7
                 )
