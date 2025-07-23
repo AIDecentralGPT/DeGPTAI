63a96fef598ca961ac5b2f666ea82e7bbc4333b1
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-07-23 12:00:23 +0800

    update password eye svg

--- src/lib/components/wallet/OpenWalletModal.svelte
+++ src/lib/components/wallet/OpenWalletModal.svelte
@@ -171,19 +171,6 @@
                   autocomplete="current-password"
                   required
                 />
-                <!-- 开眼图标 -->
-                <button class="absolute right-3 top-1/2 -translate-y-1/2"
-                  on:click={() => {
-                  showPassword = false
-                  }}>
-                  <svg xmlns="http://www.w3.org/2000/svg" 
-                    class="h-5 w-5" 
-                    viewBox="0 0 20 20" 
-                    fill="currentColor">
-                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                    <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                  </svg>
-                </button>
               {:else}
                 <input
                   bind:value={password}
@@ -193,20 +180,56 @@
                   autocomplete="current-password"
                   required
                 />
-                <!-- 闭眼图标 -->
-                <button class="absolute right-3 top-1/2 -translate-y-1/2"
-                  on:click={() => {
-                  showPassword = true
-                  }}>
-                  <svg xmlns="http://www.w3.org/2000/svg" 
-                    class="h-5 w-5" 
-                    viewBox="0 0 20 20" 
-                    fill="currentColor">
-                    <path fill-rule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clip-rule="evenodd" />
-                    <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
-                  </svg>
-                </button>
               {/if}
+              <button
+                type="button"
+                class="absolute inset-y-0 right-0 px-3 py-2 text-sm dark:text-gray-300 dark:bg-gray-850 rounded-md"
+                on:click={() => (showPassword = !showPassword)}
+                >
+                  {#if showPassword}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 512 512"
+                      ><path
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="32"
+                        d="M255.66 112c-77.94 0-157.89 45.11-220.83 135.33a16 16 0 0 0-.27 17.77C82.92 340.8 161.8 400 255.66 400c92.84 0 173.34-59.38 221.79-135.25a16.14 16.14 0 0 0 0-17.47C428.89 172.28 347.8 112 255.66 112"
+                      /><circle
+                        cx="256"
+                        cy="256"
+                        r="80"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-miterlimit="10"
+                        stroke-width="32"
+                      /></svg
+                    >
+                  {:else}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24"
+                      ><g
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        ><path
+                          d="M9.88 9.88a3 3 0 1 0 4.24 4.24m-3.39-9.04A10 10 0 0 1 12 5c7 0 10 7 10 7a13.2 13.2 0 0 1-1.67 2.68"
+                        /><path
+                          d="M6.61 6.61A13.5 13.5 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61M2 2l20 20"
+                        /></g
+                      ></svg
+                    >
+                  {/if}
+                </button>
             </div>
           </div>
         </div>
@@ -337,19 +360,6 @@
                     autocomplete="current-password"
                     required
                   />
-                  <!-- 开眼图标 -->
-                  <button class="absolute right-3 top-1/2 -translate-y-1/2"
-                    on:click={() => {
-                      showPassword = false
-                    }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" 
-                      class="h-5 w-5" 
-                      viewBox="0 0 20 20" 
-                      fill="currentColor">
-                      <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                      <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                    </svg>
-                  </button>
                 {:else}
                   <input
                     bind:value={password}
@@ -359,20 +369,56 @@
                     autocomplete="current-password"
                     required
                   />
-                  <!-- 闭眼图标 -->
-                  <button class="absolute right-3 top-1/2 -translate-y-1/2"
-                    on:click={() => {
-                      showPassword = true
-                    }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" 
-                      class="h-5 w-5" 
-                      viewBox="0 0 20 20" 
-                      fill="currentColor">
-                      <path fill-rule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clip-rule="evenodd" />
-                      <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
-                    </svg>
-                  </button>
                 {/if}
+                <button
+                  type="button"
+                  class="absolute inset-y-0 right-0 px-3 py-2 text-sm dark:text-gray-300 dark:bg-gray-850 rounded-md"
+                  on:click={() => (showPassword = !showPassword)}
+                >
+                  {#if showPassword}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 512 512"
+                      ><path
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="32"
+                        d="M255.66 112c-77.94 0-157.89 45.11-220.83 135.33a16 16 0 0 0-.27 17.77C82.92 340.8 161.8 400 255.66 400c92.84 0 173.34-59.38 221.79-135.25a16.14 16.14 0 0 0 0-17.47C428.89 172.28 347.8 112 255.66 112"
+                      /><circle
+                        cx="256"
+                        cy="256"
+                        r="80"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-miterlimit="10"
+                        stroke-width="32"
+                      /></svg
+                    >
+                  {:else}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24"
+                      ><g
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        ><path
+                          d="M9.88 9.88a3 3 0 1 0 4.24 4.24m-3.39-9.04A10 10 0 0 1 12 5c7 0 10 7 10 7a13.2 13.2 0 0 1-1.67 2.68"
+                        /><path
+                          d="M6.61 6.61A13.5 13.5 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61M2 2l20 20"
+                        /></g
+                      ></svg
+                    >
+                  {/if}
+                </button>
               </div>
             </div>
           </div>
