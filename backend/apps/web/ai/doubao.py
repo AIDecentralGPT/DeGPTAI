4a1f983bd84a46bf2bd12a0b2e09fc0b18e7ed34
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-08-07 16:18:47 +0800

    update doubao api call

--- backend/apps/web/ai/doubao.py
+++ backend/apps/web/ai/doubao.py
@@ -1,11 +1,16 @@
 import os
 import requests
+from openai import OpenAI, APIError
 from apps.web.models.aimodel import AiModelReq
 from fastapi.encoders import jsonable_encoder
-from apps.web.util.fileutils import FileUtils
 
-apiurl = "https://ark.cn-beijing.volces.com/api/v3/chat/completions"
+apiurl = "https://ark.cn-beijing.volces.com/api/v3"
 apikey = os.getenv("DOUBAO_API_KEY")
+
+client = OpenAI(
+    api_key=apikey,
+    base_url=apiurl,
+)
 
 
 class DoubaoApi:
@@ -14,31 +19,23 @@ class DoubaoApi:
         return model in models
    
     def completion(self, param: AiModelReq):
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {apikey}"
-        }
-        for message in param.messages:
-            if isinstance(message.content, list):
-                for file in message.content:
-                    if file["type"] == "file":
-                        file_url = file["file"]["file_data"]
-                        file["file"]["file_data"] = FileUtils.url_to_data_url(file_url)
-        data = {
-            "model": param.model,  # 假设的模型名称，请替换为实际模型名称
-            "messages": param.messages,
-            "temperature": 0.7,
-            "stream": True  # 启用流式输出
-        }
         try:
-            response = requests.post(apiurl, headers=headers, json=jsonable_encoder(data), stream=True)
-            response.raise_for_status() # 非200走异常
-            return response.iter_lines()
-        except requests.exceptions.HTTPError as e:
+            completion = client.chat.completions.create(
+                model=param.model,
+                messages=param.messages,
+                stream=param.stream,  #流模式
+                extra_body={
+                    "thinking": {
+                        "type": "enabled" if param.enable_thinking else "disabled"
+                    }
+                }
+            )
+        except APIError as e:
             print("==========DoubaoApi Error===========", e)
-            return None
+            completion = None
         except Exception as e:
             print("==========DoubaoApi Error===========", e)
-            return None
-   
+            completion = None
+        return completion
+
 DoubaoApiInstance = DoubaoApi()
