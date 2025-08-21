50e76623cf0ef1cf4f83968d938bf6317bb1114b
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-08-21 12:50:50 +0800

    update default models

--- src/lib/components/wallet/GetRewards.svelte
+++ src/lib/components/wallet/GetRewards.svelte
@@ -83,8 +83,9 @@
     let selmodels = $settings?.models ?? ['deepseek-chat'];
     if (selmodels.length > 0) {
       modObj = $models.filter(item => selmodels.includes(item?.model));
-    } else {
-      modObj = $models.find(item => item?.model === 'deepseek-chat');
+    }
+    if (modObj.length == 0) {
+      modObj = [$models.find(item => item?.model === 'deepseek-chat')];
     }
   }
 </script>
