962baa7152ea75bdb4537d89f76b235f6052f686
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-12-04 19:15:48 +0800

    update deepseek version

--- backend/apps/web/ai/deepseek.py
+++ backend/apps/web/ai/deepseek.py
@@ -4,7 +4,6 @@ from apps.web.models.aimodel import AiModelReq, AiMessageModel
 
 apiurl = "https://api.deepseek.com"
 apiurlbate = "https://api.deepseek.com/beta"
-apiurlspeciale="https://api.deepseek.com/v3.2_speciale_expires_on_20251215"
 apikey = os.getenv("DEEPSEEK_API_KEY")
 client = OpenAI(
     api_key=apikey,
@@ -16,40 +15,28 @@ clientbate = OpenAI(
     base_url=apiurlbate,
 )
 
-clientspeciale = OpenAI(
-    api_key=apikey,
-    base_url=apiurlspeciale,
-)
-
 
 class DeepseekApi:
     def check_model(self, model: str):
         models = ["deepseek-chat", "deepseek-reasoner"]
         return model in models
-   
+
     def completion(self, param: AiModelReq):
         try:
-            if param.enable_thinking:
-                completion = clientspeciale.chat.completions.create(
+            if param.reload:
+                param.messages[-1].prefix = True
+                completion = clientbate.chat.completions.create(
                     model=param.model,
                     messages=param.messages,
-                    stream=param.stream,  #流模式
+                    stream=param.stream,  # 流模式
+                    stop=["```"],
                 )
             else:
-                if param.reload:
-                    param.messages[-1].prefix = True
-                    completion = clientbate.chat.completions.create(
-                        model=param.model,
-                        messages=param.messages,
-                        stream=param.stream,  #流模式
-                        stop=["```"],
-                    )
-                else:
-                    completion = client.chat.completions.create(
-                        model=param.model,
-                        messages=param.messages,
-                        stream=param.stream,  #流模式
-                    )
+                completion = client.chat.completions.create(
+                    model=param.model,
+                    messages=param.messages,
+                    stream=param.stream,  # 流模式
+                )
 
         except APIError as e:
             print("==========DeepseekApi Error===========", e)
@@ -58,5 +45,6 @@ class DeepseekApi:
             print("==========DeepseekApi Error===========", e)
             completion = None
         return completion
-   
+
+
 DeepseekApiInstance = DeepseekApi()
